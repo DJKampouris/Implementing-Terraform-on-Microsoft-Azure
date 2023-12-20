20e4180466d058c81759cb053559bb2f25c6170a
Dimitri Hudon-Kampouris <dimitri@ehkdata.com>
2023-12-20 10:39:44 +0100

    WIP

--- 9-app-deploy/main.tf
+++ 9-app-deploy/main.tf
@@ -6,7 +6,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.0"
+      version = "~> 3.0"
     }
   }
 
@@ -73,7 +73,7 @@ data "terraform_remote_state" "networking" {
 
 data "azurerm_subnet" "app" {
   name                 = "app"
-  virtual_network_name = data.terraform_remote_state.networking.outputs.vnet_name
+  virtual_network_name = data.terraform_remote_state.networking.outputs.virtual_network_name
   resource_group_name  = data.terraform_remote_state.networking.outputs.resource_group_name
 }
 
